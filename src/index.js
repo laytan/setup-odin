e985f27fcdfdcb9466ccde18e8315515bca0c088
Laytan <laytanlaats@hotmail.com>
2024-08-03 23:12:12 +0200

    fix: updates to support latest release (#40)
    * fix: weird LLVM library in latest release
    
    * ci: skip asm because of upstream issue
    
    * ci: skip llvm 13 because of upstream bug
    
    * fix: windows release changes

--- src/index.js
+++ src/index.js
@@ -317,7 +317,14 @@ async function downloadRelease(inputs) {
 
   // NOTE: after dev-2024-06 releases don't seem to be doubly zipped anymore
   // but do still have the nested dist folder we need to move.
-  const maybeNestedDistName = (os.platform() == 'win32') ? '/windows_artifacts' : '/dist';
+
+  // NOTE: dev-2024-07 has 'windows_artifacts' on windows, all others have 'dist'.
+
+  let maybeNestedDistName = 'windows_artifacts';
+  if (!fs.existsSync(`${common.odinPath()}/${maybeNestedDistName}`)) {
+    maybeNestedDistName = 'dist';
+  }
+
   const maybeNestedDist = `${common.odinPath()}/${maybeNestedDistName}`;
   if (fs.existsSync(maybeNestedDist)) {
     core.info('Moving dist folder');
@@ -326,12 +333,12 @@ async function downloadRelease(inputs) {
     await Promise.all(entries.map((entry) => io.mv(`${maybeNestedDist}/${entry}`, `${common.odinPath()}/${entry}`)));
 
     // NOTE: somehow after dev-2024-06 we also need to make it executable again...
-    makeCompilerExecutable();
+    finalizeRelease();
 
     return true;
   }
 
-  makeCompilerExecutable();
+  finalizeRelease();
 
   // NOTE: Older releases of darwin did not bundle LLVM, from 2023-10 onwards it needs llvm 13 installed via brew.
   if (os.platform() == 'darwin') {
@@ -341,13 +348,20 @@ async function downloadRelease(inputs) {
   return true;
 }
 
-// NOTE: after dev-2024-03 these releases have the executable permission by default, we still 
-// chmod to support older releases.
-async function makeCompilerExecutable() {
+async function finalizeRelease() {
+  // NOTE: after dev-2024-03 these releases have the executable permission by default, we still 
+  // chmod to support older releases.
   if (os.platform() == 'linux' || os.platform() == 'darwin') {
     const code = await exec.exec(`chmod +x ${common.odinPath()}/odin`);
     if (code != 0) {
       core.warning(`Exit code ${code} making the compiler executable`);
+    }
+  }
+
+  if (os.platform() == 'linux') {
+    const possibleLibLLVMToRename = `${common.odinPath()}/libLLVM-18.so.1`;
+    if (fs.existsSync(possibleLibLLVMToRename)) {
+      fs.cpSync(possibleLibLLVMToRename, `${common.odinPath()}/libLLVM-18.so.18.1`);
     }
   }
 }
